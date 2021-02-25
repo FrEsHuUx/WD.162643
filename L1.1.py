4ce652d6db009a148ffadf6e060630aac4ee3b67
FrEsHuUx <jakub@klobuszenski.eu>
2021-02-25 17:11:45 +0100

    zadanka + L z lekcji

---
+++ L1.1.py
@@ -0,0 +1,35 @@
+from math import *
+a = 10
+b = 5
+c = 2
+
+dodawanie = a + b
+mnozenie = a * b
+dzielenie = a/b
+dzielenie_calkowite = b // c
+potega = c ** b
+
+print(dodawanie, mnozenie)
+print(mnozenie)
+print(dzielenie)
+print(dzielenie_calkowite)
+print(potega)
+print(pow(8, 2))
+a *= c
+
+print(sqrt(9))
+print(pi)
+
+
+print(u'inżynieria systemów informatycznych')
+print('wynik dzialania jest rowny a = %(zm)d' % {'zm' : 12})
+napis = 'isi'
+grupa = 2
+
+print('specjalizacja %s, grupa %d' % (napis, grupa))
+
+d = 5
+e = 3
+z = 5 - 3
+
+print('Wynik dzialania %(z1)d-%(z2)d=%(z3)d' % {'z1': d, 'z2': e, 'z3': z})
